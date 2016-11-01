21ac47db9333db69d98d2179fc35f69e92c269e0
greg mckinney <gmck95@msn.com>
2016-11-01 13:09:20 -0400

    add base project

---
+++ MonsterWeb.Logic/TestData.cs
@@ -0,0 +1,19 @@
+﻿using MonsterApp.DataClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterWeb.Logic
+{
+  public class TestData
+  {
+    public MonsterService data = new MonsterService();
+
+    public void GetGenders()
+    {
+      data.GetGender();
+    }
+  }  
+}
